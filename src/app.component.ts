e6bb9ae859861ab2bb950b58146822f1286b7168
Carlos Filoteo <carlos_filoteo@byu.edu>
2016-02-01 10:40:28 -0700

    Add properties to AppComponent. Add more complex template string.

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,6 +2,37 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:
+`<div id="layout" class="pure-g">
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+        <div class="header">
+            <h1 class="brand-title">{{ name }}</h1>
+            <h2 class="brand-tagline">{{ email }}</h2>
+        </div>
+    </div>
+
+    <main class="content pure-u-1 pure-u-md-3-4">
+        <div>
+            <!-- Posts wrapper -->
+            <div class="posts">
+                <h1 class="content-subhead">Experience</h1>
+
+                <!-- Single post -->
+                <section class="post">
+                    <header class="post-header">
+                        <h2 class="post-title">Job Position</h2>
+                        <p class="post-meta">Job metadata</p>
+                    </header>
+                    <div class="post-description">
+                        <p>Job description</p>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </main>
+</div>`
 })
-export class AppComponent { }
+export class AppComponent {
+    public name = 'Your name';
+    public email = 'youremail@address.com';
+}
